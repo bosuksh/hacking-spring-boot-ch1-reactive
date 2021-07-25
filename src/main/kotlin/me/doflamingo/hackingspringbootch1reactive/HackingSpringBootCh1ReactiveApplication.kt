8969c7248ace87bd9786c1f3f4585cc7e376f45c
bosuksh <bosuksh@gmail.com>
2021-07-25 15:56:59 +0900

    Initial commit

---
+++ src/main/kotlin/me/doflamingo/hackingspringbootch1reactive/HackingSpringBootCh1ReactiveApplication.kt
@@ -0,0 +1,11 @@
+package me.doflamingo.hackingspringbootch1reactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HackingSpringBootCh1ReactiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<HackingSpringBootCh1ReactiveApplication>(*args)
+}
